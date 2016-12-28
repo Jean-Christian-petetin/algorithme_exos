4b30a36d57d4046c35cd482b0e65cfd7e63c54d4
jean-christian-petetin <jeanchristianpetetin@gmail.com>
2016-12-28 16:53:03 +0100

    ajout de l'exo6 en ruby, python, php

--- exo6/exo6.py
+++ exo6/exo6.py
@@ -2,9 +2,27 @@
 #JesuScript
 #beweb
 
-print("Bienvenue cher le perroquet du capitaine")
-perroquet = raw_input("entrez une phrase ou un mot: ")
-print(str(perroquet))
-print("pour quitter appuyer sur y")
+quitter = False
+oui = "o"
+non = "n"
+reponse = ""
+flag = False
 
+while quitter == False:
+	print("Bienvenue chez le perroquet du capitaine")
+	perroquet = raw_input("entrez une phrase ou un mot: ")
+	print(str(perroquet))
+	flag = True
+
+	while flag == True:
+		reponse = raw_input("Voulez vous quitter o/n ?")
+
+		if oui == reponse.lower():
+			quitter = True
+			flag = False
+		elif non == reponse.lower():
+			print("non")
+			flag = False
+		else:
+			print("Moi y a na pas comprendre ce que toi vouloir me dire")
 
